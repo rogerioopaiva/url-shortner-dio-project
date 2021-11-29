8d678bb026a32fcecedf9170c040e2621c93dd89
Rogério Paiva <rogerioopaiva36@gmail.com>
2021-11-29 19:00:29 -0300

    Realizacao do projeto encurtador do url

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:5001',
+    MONGO_CONNECTION:
+    'mongodb+srv://admin:admin@url-shortener-dio.veapo.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+}
